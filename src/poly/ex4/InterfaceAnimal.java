68a8264f50af7d73c75ba1b40039ad86ce54044f
mygoaway <mygoaway2@gmail.com>
2024-05-07 15:42:49 +0900

    섹션 11. 다형성 2 강의 후 재커밋

--- src/poly/ex4/InterfaceAnimal.java
+++ src/poly/ex4/InterfaceAnimal.java
@@ -1,6 +1,9 @@
 package poly.ex4;
 
 public interface InterfaceAnimal {
+    // 인터페이스에서 상수 선언 가능
+    double PIE = 3.14; // public static final 생략 가능
+
     void sound(); // public abstract 생략
     void move();  // public abstract 생략
 }
